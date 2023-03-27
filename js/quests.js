e8841f505cf091fa6b8a0084f26ebd5969212b02
jzamorano20 <126306433+jzamorano20@users.noreply.github.com>
2023-03-27 18:30:02 -0400

    all fixed

---
+++ js/quests.js
@@ -0,0 +1,112 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many bodies can be in the index.html?",
+        answer: "One",
+        options: [
+            "One",
+            "Two",
+            "Four",
+            "As many as you want"
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within?",
+        answer: "Curly Brackets",
+        options: [
+            "Commas",
+            "Quotes",
+            "Curly Brackets",
+            "Parentheses"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the acronym for the Document Obeject Model?",
+        answer: "DOM",
+        options: [
+            "OOM",
+            "DOM",
+            "CSS",
+            "IDK"]
+    },
+    {
+        numb: 6,
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answer: "Var",
+        options: [
+            "Var",
+            "NaN",
+            "For",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 7,
+        question: "How do you call a function in javascript?",
+        answer: "myfunction()",
+        options: [
+            "Call myfunction",
+            "myfunction",
+            "call function",
+            "call.myfunction()"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What tag do you need in your HTML to link the Javascript?",
+        answer: "'<script>'",
+        options: [
+            "<\'javascript\'>",
+            "<\'js\'>",
+            "<\'link\'>",
+            "&lt; script"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the starter code need to wireframe a website?",
+        answer: "HTML",
+        options: [
+            "CSS",
+            "HTML",
+            "Javascript",
+            "Develop"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is our Instructors Name",
+        answer: "JD",
+        options: [
+            "JD",
+            "Justyn",
+            "Paul",
+            "John"
+        ]
+    },
+]
